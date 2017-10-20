0739544d78df984749ca5d346eb288a2de5126b6
RudyRed <willgross94@gmail.com>
2017-10-20 16:01:28 -0700

    Completed clearing and adding messages from DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,8 +7,31 @@ $( document ).ready(function() {
   app.send = function (data) {
     $.ajax({
       type: 'POST',
-      url: '/chatterbox/classes/messages',
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
       data: data
     });
   };
+  
+  app.fetch = function () {
+    $.ajax({
+      type: 'GET',
+      url: undefined,
+      success: function (resp) {
+        console.log(resp);
+      },
+      error: function () {
+        console.log('error');
+      }
+    });
+  };
+
+  app.clearMessages = function () {
+    $('#chats').empty();
+  };
+
+  app.renderMessage = function ({text}) {
+    $('#chats').append(`<p>${text}</p>`);
+  }; 
+
+
 });
