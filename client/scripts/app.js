ef85257e4d5b08609afa40f7d57b96eb55697bcd
RudyRed <willgross94@gmail.com>
2017-10-20 15:15:40 -0700

    completed chatterbox init, app behavior sending

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,14 @@
-// YOUR CODE HERE:
+$( document ).ready(function() {
+
+  window.app = {};
+  
+  app.init = function () {};
+  
+  app.send = function (data) {
+    $.ajax({
+      type: 'POST',
+      url: '/chatterbox/classes/messages',
+      data: data
+    });
+  };
+});
