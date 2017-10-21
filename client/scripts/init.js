8b9b0836f3aab9927a27870c049e54e76ed02efa
RudyRed <willgross94@gmail.com>
2017-10-20 19:58:00 -0700

    Can return messages from server

--- client/scripts/init.js
+++ client/scripts/init.js
@@ -1,8 +1,9 @@
 //// Initializatoin file that will begin fetching tweets
 
 
+setInterval( function () {
+  app.clearMessages();
+  app.fetch();
+  app.savedMessages.forEach(x => app.renderMessage(x));
 
-
-app.fetch();
-app.savedMessages.forEach(x => app.renderMessage(x));
-
+}, 1000);
